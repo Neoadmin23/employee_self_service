634421e9d48374b1c0219753ae2a6a3df9f5505c
SyedAthar1251 <sayedather@neotec.ai>
2026-07-01 16:01:39 +0300

    login fix

--- employee_self_service/employee_self_service/doctype/ess_admin_user/ess_admin_user.py
+++ employee_self_service/employee_self_service/doctype/ess_admin_user/ess_admin_user.py
@@ -12,3 +12,40 @@ class ESSAdminUser(Document):
             user_full_name = frappe.db.get_value("User", self.user, "full_name")
             if user_full_name:
                 self.full_name = user_full_name
+
+
+@frappe.whitelist(allow_guest=True)
+def get_admin_user(user):
+    try:
+        admin_user = frappe.get_all(
+            "ESS Admin User",
+            filters={"user": user, "is_active": 1},
+            fields=["user", "full_name", "email", "role", "is_active"],
+        )
+
+        if not admin_user:
+            return {
+                "success": False,
+                "message": "User is not admin. Please register it as Company admin.",
+                "data": None,
+            }
+
+        admin_user = admin_user[0]
+        admin_user["allowed_companies"] = frappe.get_all(
+            "ESS Admin User Company",
+            filters={"parent": admin_user["user"]},
+            fields=["company"],
+        )
+
+        return {
+            "success": True,
+            "message": "Admin User Get Successfully",
+            "data": admin_user,
+        }
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Get Admin User Error")
+        return {
+            "success": False,
+            "message": str(e),
+            "data": None,
+        }
